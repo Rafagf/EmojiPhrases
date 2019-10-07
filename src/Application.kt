9dd702786a3e9089df12bfb6200dd4228c6a43fb
Rafa <rafaelgarciafernandez@Rafaels-MacBook-Pro.local>
2019-10-07 17:34:51 +0100

    home and about frontend

--- src/Application.kt
+++ src/Application.kt
@@ -1,10 +1,17 @@
 package com.rafag
 
+import com.rafag.api.*
+import com.rafag.repository.*
+import com.rafag.webapp.*
 import com.ryanharter.ktor.moshi.*
+import freemarker.cache.*
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.freemarker.*
 import io.ktor.http.*
 import io.ktor.response.*
+import io.ktor.routing.*
+import webapp.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -12,6 +19,11 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     installFeatures()
+    val repository = Repository(MemoryDataSource())
+    routing {
+        home()
+        about()
+    }
 }
 
 private fun Application.installFeatures() {
@@ -25,8 +37,5 @@ private fun Application.installFeatures() {
             )
 
         }
-    }
-    install(ContentNegotiation) {
-        moshi()
     }
 }
---
+++ src/webapp/About.kt
@@ -0,0 +1,13 @@
+package webapp
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+const val ABOUT = "about"
+
+fun Route.about() {
+    get(ABOUT) {
+        call.respondText("Here goes the about page")
+    }
+}
